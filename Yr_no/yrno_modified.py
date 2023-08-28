3ff25488f658cf51c58d874e8a6911c6c0affade
Xavier <allegre.xavier@gmail.com>
2023-08-28 11:05:56 +0200

    Saves time, temp, precip into dictionaries in list
    Modification of yrno_modified

--- Yr_no/yrno_modified.py
+++ Yr_no/yrno_modified.py
@@ -41,32 +41,23 @@ while True:
     js = json.loads(result)
     #print(json.dumps(js, indent=4)) #:print the json
     
-    """store the data in semi-permanent storage"""
+    """store relevant data in semi-permanent storage"""
     
     time = list()
-    for x in js["properties"]["timeseries"][0:87]:
+    for x in js["properties"]["timeseries"][0:55]:
         time.append(x["time"])
     print(time)
-            
-    #print(time)
-    #time = js["properties"]["timeseries"][0]["time"]
-    #temperature = js["properties"]["timeseries"][0]["data"]["instant"]["details"]["air_temperature"]
-    #precipitation = js["properties"]["timeseries"][0]["data"]["next_1_hours"]["details"]["precipitation_amount"]
-    #time_1 = js["properties"]["timeseries"][6]["time"]
-    #temperature_1 = js["properties"]["timeseries"][6]["data"]["instant"]["details"]["air_temperature"]
-    #precipitation_1 = js["properties"]["timeseries"][6]["data"]["next_1_hours"]["details"]["precipitation_amount"]
-    #time_2 = js["properties"]["timeseries"][12]["time"]
-    #temperature_2 = js["properties"]["timeseries"][12]["data"]["instant"]["details"]["air_temperature"]
-    #precipitation_2 = js["properties"]["timeseries"][12]["data"]["next_1_hours"]["details"]["precipitation_amount"]
-    #time_3 = js["properties"]["timeseries"][18]["time"]
-    #temperature_3 = js["properties"]["timeseries"][18]["data"]["instant"]["details"]["air_temperature"]
-    #precipitation_3 = js["properties"]["timeseries"][18]["data"]["next_1_hours"]["details"]["precipitation_amount"]
-    #print("Time: ", time, "temperature: ", temperature, "Precipitation amount: ", precipitation)
-    #print("Time: ", time_1, "temperature: ", temperature_1, "Precipitation amount: ", precipitation_1)
-    #print("Time: ", time_2, "temperature: ", temperature_2, "Precipitation amount: ", precipitation_2)
-    #print("Time: ", time_3, "temperature: ", temperature_3, "Precipitation amount: ", precipitation_3)
+    temperature = list()
+    for y in js["properties"]["timeseries"][0:55]:
+        temperature.append(y["data"]["instant"]["details"]["air_temperature"])
+    print(temperature)
+    precipitation = list()
+    for p in js["properties"]["timeseries"][0:55]:
+        precipitation.append(p["data"]["next_1_hours"]["details"]["precipitation_amount"])
+    print(precipitation)
     
-    #weather['time'] = time
-    #weather['temperature'] = temperature
-    #weather['precipitation'] = precipitation
-    #print(weather)
+    weather = [{'time':tim, 'temperature': temp, 'precipitation': precip}
+        for tim, temp, precip in zip(time, temperature, precipitation)
+       ]
+    
+    print(weather)
